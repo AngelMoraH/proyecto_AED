4ab205fd6d3997e394fff232e6c8e111a3429cc9
ChuSebastian <sebastian.chu@utec.edu.pe>
2023-05-27 18:41:21 -0500

    formatted

--- Block.cpp
+++ Block.cpp
@@ -5,44 +5,52 @@
 
 using namespace std;
 
-class Block{
-    private:
-        int index;
-        string* prev_hash_code;
-        string hash_code;
-    public:
+class Block
+{
+private:
+    int index;
+    string *prev_hash_code;
+    string hash_code;
 
-    vector<Transaction> transactions;  //vector of transaction of fixed size
+public:
+    vector<Transaction> transactions; // vector of transaction of fixed size
 
     Block() = default;
 
-    Block(int index, vector<Transaction> transactions, string* prev_hash_code){ // [this need to change when Transaction is fully implemented, we are assuming string for simplicity]
+    Block(int index, vector<Transaction> transactions, string *prev_hash_code)
+    { // [this need to change when Transaction is fully implemented, we are assuming string for simplicity]
         this->index = index;
-        this->transactions = std::move(transactions); 
+        this->transactions = std::move(transactions);
         this->prev_hash_code = prev_hash_code;
         this->hash_code = generate_hash_code();
     }
 
-    string generate_hash_code(){
+    string generate_hash_code()
+    {
         string transact;
-        for(auto & transaction : transactions){
+        for (auto &transaction : transactions)
+        {
             transact += transaction.get_data_as_string();
         }
     }
 
-    string get_hash_code(){
+    string get_hash_code()
+    {
         return hash_code;
     }
 
-    string get_prev_hash_code(){
+    string get_prev_hash_code()
+    {
         return (*prev_hash_code);
     }
 
-    bool is_valid(){
+    bool is_valid()
+    {
         return (hash_code == generate_hash_code());
     }
 
-    void short_display(){
+    void short_display()
+    {
         cout << "       Index:              " << index << endl;
         cout << "       Hash code:          " << hash_code << endl;
         cout << "       Prev. hash code:    " << *prev_hash_code << endl;
